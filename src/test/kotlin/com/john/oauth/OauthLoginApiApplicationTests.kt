1fcf0a38a362645ce6ce07464954dca00b52b3f0
ParkYunHo <qkrdbsgh0921@gmail.com>
2023-01-31 21:09:55 +0900

    init

---
+++ src/test/kotlin/com/john/oauth/OauthLoginApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.john.oauth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OauthLoginApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
